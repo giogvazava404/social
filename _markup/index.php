f3fd4418f7615cf84a14d355c40439cfd9482d9f
social <web.gvazava@gmail.com>
2014-09-26 23:12:01 +0400

    top done

--- _markup/index.php
+++ _markup/index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="ka">
 <head>
-	<title>Murck up</title>
-	<link href="public/stylesheets/general.css" rel="stylesheet" type="text/css" />
-	<script src="public/scripts/jquery.js" type="text/javascript"></script>
-	<script src="public/scripts/general.js" type="text/javascript"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<base href="/social/_markup/" />
+<meta name="description" content="test decription">
+<meta name="keywords" content="test key work">
+<meta name="author" content="გიორგი გვაზავა">
+<title>ყიდვა გაყიდვა</title>
+<meta name="viewport" content="width=device-width">
+<link href="public/stylesheets/general.css" rel="stylesheet" type="text/css" />
+<script src="public/scripts/jquery.js" type="text/javascript"></script>
+<script src="public/scripts/general.js" type="text/javascript"></script>
 </head>
 <body>
-hey there
+	<div class="top">
+		<div class="center">
+			<div class="newsSlide">
+				<span></span> 
+			</div>
+			
+			<div class="userArea">
+				<div class="list myFavorites"><div class="span"></div><a href="#">ფავორიტები (0)</a></div>
+				<div class="list myAccount"><div class="span"></div><a href="#">ჩემი ანგარიში</a></div>
+			<//div>
+		</div>
+	</div>
 </body>
 </html> 
--- _markup/public/scripts/general.js
+++ _markup/public/scripts/general.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-	//alert("Jquery works fine !");
+	
+	topSlide("topMassage.php",".top .center .newsSlide span",0);
+	
 });
+
+
+
+function topSlide(file_name,insertx,itemx)
+{
+	var hr = new XMLHttpRequest();
+	hr.open("GET","public/json/"+file_name,true);
+	hr.setRequestHeader("Content-type","application/json",true);
+	hr.onreadystatechange = function()
+	{
+		var data = JSON.parse(hr.responseText);
+		for(var obj in data)
+		{
+			var o_q = data[obj][itemx].w;
+			var ln = data[obj][itemx].l;
+		}
+		
+		$(insertx).fadeOut("slow", function(){
+			$(insertx).html(o_q);
+			$(insertx).fadeIn("slow");			
+		});
+		
+	}
+	hr.send();
+}
